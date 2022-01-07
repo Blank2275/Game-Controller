f56b6f789b571d982c57e00f93a92d1ab09a247b
Connor <cfr3.14156@gmail.com>
2022-01-07 09:35:27 -0700

    Initial Commit

---
+++ Game-Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game-Controller
+//
+//  Created by Connor Reed on 1/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
